0ccb01506891f8819715446c2ee34d40bf8b0f8b
Neetish Raj <neetishop@gmail.com>
2022-08-05 22:55:09 +0530

    Added notes on OOPS basics and this

---
+++ JS/oops.js
@@ -0,0 +1,27 @@
+
+function Car(engine, model, year) {
+    this.engine = engine;
+    this.model = model;
+    this.year = year;
+}
+
+
+// const car1 = {
+//     engine: "800cc",
+//     model: "Maruti 800",
+//     year: 2006
+// }
+
+const car1 = new Car("800cc", "Maruti 800", 2006);
+
+// const car2 = {
+//     engine: "1.2L",
+//     model: "Honda Jazz",
+//     year: 2018
+// }
+
+const car2 = new Car("1.2L", "Honda Jazz", 2018);
+
+
+console.log(car1);
+console.log(car2);
---
+++ JS/this.js
@@ -0,0 +1,25 @@
+/**
+ * 'this' always points to the object in current lexical scope.
+ * 
+ * Therefore it's a convenient way to access the current object
+ * we are working with without worrying about it's name or refernce.
+ * 
+ */
+
+const obj = {
+    name:  'John',
+    print: function() {
+        // here 'this' is pointing to the obj class
+        ;
+        console.log(obj);
+    }
+};
+
+console.log(obj);
+
+obj.print();
+
+// Here 'this' is pointing to the global window object
+console.log(this);
+
+console.log("hello");
