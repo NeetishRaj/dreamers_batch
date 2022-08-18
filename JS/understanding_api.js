d094c40ac665994e0e73c9d9a8b59b722b3e11aa
Neetish Raj <neetishop@gmail.com>
2022-08-18 23:02:24 +0530

    Added notes on DOM basics and selectors

---
+++ JS/understanding_api.js
@@ -0,0 +1,30 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+
+// Arithmatic API
+
+const Arithmatic = {
+    add: function (num1, num2) {
+        return num1 + num2;
+    },
+
+    sub: function (num1, num2) {
+        return num1 - num2;
+    }
+}
+
+
+console.log(Arithmatic.add(2, 5)); //7 
+console.log(Arithmatic.sub(10, 5)); //5 
+
+
+
+// Other examples of API
+
+// Math API - PI, sqrt(), pow(), sin()
