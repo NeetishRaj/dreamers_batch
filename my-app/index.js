75097e3205a71fc5e8fff08d3d11a44bd97d59f4
Neetish Raj <neetishop@gmail.com>
2022-09-28 21:39:57 +0530

    react setup without create-react-app

---
+++ my-app/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import reactDom from 'react-dom';
+import App from './src/components/App';
+
+reactDom.render(<App />, document.getElementById("root"));
---
+++ my-app/src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () =>{
+    return (
+        <div>Hell there!</div>
+    )
+}
+
+export default App
