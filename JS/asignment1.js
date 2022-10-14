e16439431eda31848255e7b89eae42ce1432c87c
Neetish Raj <neetishop@gmail.com>
2022-10-14 22:27:45 +0530

    Added notes for today

---
+++ JS/asignment1.js
@@ -0,0 +1,47 @@
+// Filter the Indian cities from this array
+// Sort it in acsending alphabetical order based on city names
+const cities = [
+    { name: 'Goa', country: 'India' },
+    { name: 'Amsterdam', country: 'Netherlands' },
+    { name: 'New York', country: 'USA' },
+    { name: 'Darjeeling', country: 'India' },
+    { name: 'Tokyo', country: 'Japan' },
+    { name: 'Lonavala', country: 'India' },
+    { name: 'Brandenburg Gate', country: 'Germany' },
+    { name: 'Reichstag Building', country: 'Germany' },
+    { name: 'Museum Island', country: 'Germany' },
+    { name: 'Munnar', country: 'India' },
+    { name: 'Leh Ladakh', country: 'India' },
+    { name: 'Goa', country: 'India' },
+    { name: 'Agra', country: 'India' },
+    { name: 'Dalhousie', country: 'India' },
+    { name: 'Coorg', country: 'India' },
+    { name: 'Rome', country: 'Italy' },
+    { name: 'Milan', country: 'Italy' },
+    { name: 'Venice', country: 'Italy' },
+    { name: 'Varanasai', country: 'India' },
+    { name: 'Jaipur', country: 'India' },
+    { name: 'The Hofburg', country: 'Austria' },
+    { name: 'Belvedere Palace', country: 'Austria' },
+    { name: 'St. Stephen Cathedral', country: 'Austria' },
+    { name: 'Kahna National Park', country: 'India' },
+    { name: 'Amritsar', country: 'India' },
+    { name: 'Mussoorie', country: 'India' },
+    { name: 'Mount Abu', country: 'India' },
+    { name: 'Tirupati', country: 'India' },
+]
+
+const result = cities
+    .filter(city => city.country === "India")
+    .sort((a, b) => {
+        if(a.name < b.name) {
+            return -1;
+        } else if( a.name > b.name) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+
+
+console.log(result);
